0a3dc2ebc7dd0cfc1352a76be422c0dc49bf1b6c
Mark Anna <anna.mark.spb@gmail.com>
2020-02-18 21:21:45 +0300

    topic6.1

---
+++ topic6/select max messeges.sql
@@ -0,0 +1,37 @@
+/*Пусть задан некоторый пользователь. Из всех друзей этого пользователя найдите человека, который больше всех общался с нашим пользователем.*/
+
+use vk;
+
+select 
+	count(*),
+	media_type_id, 
+	(select name from media_types where id = media.media_type_id ) as 'media type name'
+from media
+group by media_type_id
+
+select sum(mess_to) as max_messeges,
+mess_user
+from 
+(select 
+	count(*) as mess_to,
+	from_user_id as mess_user
+from messages
+where
+	to_user_id = 1
+group by from_user_id
+union
+select 
+	count(*) as mess_from,
+	to_user_id as mess_user
+from messages
+where
+	from_user_id  = 1
+group by to_user_id) src
+group by mess_user
+order by max_messeges DESC
+LIMIT 1;
+
+	
+	
+	
+	
